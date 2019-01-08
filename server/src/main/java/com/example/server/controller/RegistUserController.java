bbe6bc4e4421d64cc1943172e9f9a9206bd403e6
Hwang, Sunghyun <sunghyun.hwang.ru@hitachi.com>
2019-01-09 08:51:17 +0900

    check import

--- server/src/main/java/com/example/server/controller/RegistUserController.java
+++ server/src/main/java/com/example/server/controller/RegistUserController.java
@@ -8,7 +8,6 @@ import com.example.server.service.UserService;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
